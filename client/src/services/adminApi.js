1f9d2ebb81d90716f68a6c34e439c32af8cca63c
nathira99 <nathirafarveen99@gmail.com>
2026-01-16 22:00:26 +0530

    login page --update

--- client/src/services/adminApi.js
+++ client/src/services/adminApi.js
@@ -3,14 +3,14 @@ import API from "../config/api";
 // const API = process.env.REACT_APP_API_URL;
 
 export const getStats = (token) =>
-  axios.get(`${API}/stats`, {
+  axios.get(`${API}/api/adminstats`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
 
 export const getPayments = (token, page = 1, limit = 10) =>
-  axios.get(`${API}/payments?page=${page}&limit=${limit}`, {
+  axios.get(`${API}/api/admin/payments?page=${page}&limit=${limit}`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },

1ccdb60cb7206ba5cdbb5da128be2e2b9ea6253e
nathira99 <nathirafarveen99@gmail.com>
2026-01-16 21:47:20 +0530

    login page --update

--- client/src/services/authApi.js
+++ client/src/services/authApi.js
@@ -3,11 +3,11 @@ import axios from "axios";
 const API = process.env.REACT_APP_API_URL;
 
 export const loginUser = (data) => {
-  return axios.post(`${API}/login`, data);
+  return axios.post(`${API}/api/auth/login`, data);
 };
 
 export const registerUser = (data) => {
-  return axios.post(`${API}/register`, data);
+  return axios.post(`${API}/api/auth/register`, data);
 };
 
 console.log(API);
